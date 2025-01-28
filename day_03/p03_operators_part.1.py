3889742195e14cb781e84809ef4affc31946b04c
Ahmtfrks <lusiferc679@gmail.com>
2025-01-28 22:03:06 +0100

    day04

---
+++ day_03/p03_operators_part.1.py
@@ -0,0 +1,37 @@
+#aritmatic operations
+
+a= 20
+b= 30
+
+print(a+b)
+
+#substruction
+d=b-a
+
+print(d)
+
+print(b/a) #1.5 IMPORTANT INJAVA result int but python float
+
+print(a*b) #600
+
+c=4
+
+print(b%c)
+
+print('______________________________________________________')
+
+#short-hand operations
+
+balance= 200
+
+#balance=balance+300
+
+balance+=300
+
+print(balance)
+
+language= "Python"
+
+language+=" programming language"
+
+print('______________________________________________________')
