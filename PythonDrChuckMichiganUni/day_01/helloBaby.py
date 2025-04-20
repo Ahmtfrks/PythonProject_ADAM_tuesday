a18b997b621600f5f1ee44daf34356c7252a1c97
Ahmtfrks <lusiferc679@gmail.com>
2025-04-20 10:24:27 +0200

    MochEX

---
+++ PythonDrChuckMichiganUni/day_01/helloBaby.py
@@ -0,0 +1,20 @@
+print('Halo, Me llamo Ahmet')
+
+
+#user input function--> always returns string
+
+name = input('Who are you?')
+print(f'Halo, {name}')
+
+#comma is space, one space between
+
+
+#conver elevator floors
+floor = input('Which US floor are you?')
+
+
+#int() function to convert string to integer
+floor=int(floor)-1
+
+print('Europe floor', {floor})
+
